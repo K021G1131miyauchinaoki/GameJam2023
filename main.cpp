86a37f0f4193c38ce7d07bcc0996f130ccd8275c
k021g1131@g.neec.ac.jp <mk021g1131@g.neec.ac.jp>
2023-09-05 04:22:07 +0900

    oldkeyを追加

--- main.cpp
+++ main.cpp
@@ -11,12 +11,11 @@ const int WIN_HEIGHT = 600;
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
                    _In_ int nCmdShow) {
+	// Log.texを消す処理
+	SetOutApplicationLogValidFlag(FALSE);
+
 	// ウィンドウモードに設定
 	ChangeWindowMode(TRUE);
-
-	// ウィンドウサイズを手動では変更させず、
-	// かつウィンドウサイズに合わせて拡大できないようにする
-	SetWindowSizeChangeEnableFlag(FALSE, FALSE);
 
 	// タイトルを変更
 	SetMainWindowText(TITLE);
@@ -31,37 +30,43 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SetBackgroundColor(0x00, 0x00, 0x00);
 
 	// DXlibの初期化
-	if (DxLib_Init() == -1) { return -1; }
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
 
 	// (ダブルバッファ)描画先グラフィック領域は裏面を指定
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	// 画像などのリソースデータの変数宣言と読み込み
 
-
 	// ゲームループで使う変数の宣言
 
-
+	
 	// 最新のキーボード情報用
-	char keys[256] = {0};
+	char keys[256] = { 0 };
 
 	// 1ループ(フレーム)前のキーボード情報
-	char oldkeys[256] = {0};
+	char oldkeys[256] = { 0 };
 
 	// ゲームループ
-	while (true) {
+	while (true)
+	{
+		ClearDrawScreen();
 		// 最新のキーボード情報だったものは1フレーム前のキーボード情報として保存
+		for (int i = 0; i < 256; ++i)
+		{
+			oldkeys[i] = keys[i];
+		}
 		// 最新のキーボード情報を取得
 		GetHitKeyStateAll(keys);
+		//---------  ここからにプログラムを記述  ---------//
 
-		// 画面クリア
-		ClearDrawScreen();
-		//---------  ここからプログラムを記述  ----------//
+		//更新
+		
+		//描画---------------
+		
 
-		// 更新処理
-
-
-		// 描画処理
 
 		//---------  ここまでにプログラムを記述  ---------//
 		// (ダブルバッファ)裏面
@@ -71,12 +76,14 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		WaitTimer(20);
 
 		// Windowsシステムからくる情報を処理する
-		if (ProcessMessage() == -1) {
+		if (ProcessMessage() == -1)
+		{
 			break;
 		}
 
 		// ESCキーが押されたらループから抜ける
-		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) {
+		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1)
+		{
 			break;
 		}
 	}
