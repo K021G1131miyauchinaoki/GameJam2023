f32726774260af97bc8d413f92e5f3ee8435ed2f
MiyataKouki <k021g1124@g.neec.ac.jp>
2023-09-05 12:35:40 +0900

    移動の初動が遅く段々早くなっていく(制限付き)

--- player.cpp
+++ player.cpp
@@ -4,21 +4,33 @@ void Player::Initialize()
 {
 	pos = { 225,300 };	// 中心座標
 	radius = 20;		// 半径
-	speed = 5;			// 速度
+	speed = 0;			// 速度
 	gravity = 0.0f;		// 重力
 }
 
 void Player::Update(char* keys, char* oldkey)
 {
+	//移動
 	if (keys[KEY_INPUT_RIGHT] && oldkey[KEY_INPUT_RIGHT])
 	{
-		pos.x += speed;
+		if (speed <= 5)
+		{
+			speed += 0.2f;
+		}
 	}
-	if (keys[KEY_INPUT_LEFT] && oldkey[KEY_INPUT_LEFT])
+	else if (keys[KEY_INPUT_LEFT] && oldkey[KEY_INPUT_LEFT])
 	{
-		pos.x -= speed;
+		if (speed >= -5)
+		{
+			speed -= 0.2f;
+		}
 	}
-	
+	else
+	{
+		speed = 0.0f;
+	}
+	pos.x += speed;
+	//ジャンプ
 	if (keys[KEY_INPUT_SPACE] && !oldkey[KEY_INPUT_SPACE])
 	{
 		gravity = -8.0f;
@@ -40,4 +52,5 @@ void Player::Draw()
 	DrawBox(pos.x - radius, pos.y - radius, pos.x + radius, pos.y + radius, GetColor(255, 255, 255), true);
 
 	DrawFormatString(0, 0, GetColor(255, 255, 255), "player : %f/%f\n", pos.x, pos.y);
+	DrawFormatString(0, 15, GetColor(255, 255, 255), "speed : %f\n", speed);
 }
