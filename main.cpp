09f84554fcf4a1a17c92c32105a4ad86499c59fc
kamisatomasatoshi <k021g1271@g.neec.ac.jp>
2023-09-13 12:41:40 +0900

    add me

--- main.cpp
+++ main.cpp
@@ -606,16 +606,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 		DrawGraph(back.x, back.y, backTex, TRUE);
 		DrawGraph(sceneTitle.x, sceneTitle.y, sceneTex, TRUE);
-
-
-		
-
-		for (int i = 0; i < 30; i++)
-		{
-			//DrawLine(0, 1 * (i * 55.3), WIN_WIDTH, 1 * (i * 55.3), GetColor(255, 0, 0));
-			//DrawLine(1 * (i * 55.6), 0, 1 * (i * 55.6), WIN_HEIGHT, GetColor(255, 0, 0));
-		}
-		
 		 
 
 		//---------  ここまでにプログラムを記述  ---------//
